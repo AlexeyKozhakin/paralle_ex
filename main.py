d0a57bef1afbbf9b4929f85207edf1a0f24b0cb4
Алексей Кожакин <alexeykozhakin@yandex.ru>
2022-05-10 22:51:22 +0200

    version 2.0 - example from course

--- main.py
+++ main.py
@@ -1,18 +1,85 @@
-import tensorflow as tf
-import numpy as np
-import os
+# Библиотека работы с массивами
+import numpy as np   
 
-os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+# Библиотека для работы с таблицами
+import pandas as pd                       
 
-model = tf.keras.models.Sequential()
+# Последовательная модель НС
+from tensorflow.keras.models import Sequential 
 
-model.add(tf.keras.layers.Dense(1,input_dim=1))
+# Основные слои
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 
-model.compile(loss='mean_squared_error', optimizer='adam')
+# Слой задания активационной функции <----- !!!
+from tensorflow.keras.layers import Activation
 
-xs = np.array([-1, 0, 1, 2, 3, 4])
-ys = np.array([-3, -1, 1, 3, 5, 7])
-model.fit(xs, ys, epochs=500)
+# Утилиты предобработки данных
+from tensorflow.keras import utils
 
-to_predict = np.array([10, 11, 12 ,13])
-print(model.predict(to_predict))
+# Оптимизаторы
+from tensorflow.keras.optimizers import Adam
+
+# Разделение на обучающую и проверочную/тестовую выборку
+from sklearn.model_selection import train_test_split
+
+# Рисование графиков
+import matplotlib.pyplot as plt
+
+import gdown
+
+#gdown.download('https://storage.yandexcloud.net/aiueducation/Content/base/l4/sonar.csv', None, quiet=True)
+
+df = pd.read_csv("sonar.csv", header=None)
+
+print(df.shape)
+
+df.head()
+
+dataset = df.replace('R', 1.).replace('M', 0.).astype(float).to_numpy()
+
+x_data = dataset[:, :60]
+y_data = dataset[:, 60]
+
+x_train, x_test, y_train, y_test = train_test_split(x_data, # набор параметров
+                                                    y_data, # набор меток классов
+                                                    test_size=0.2, # процент в тестовую
+                                                    shuffle=True, #  перемешивание
+                                                    random_state=3) # воспроизводимость
+
+# Выведем размерность полученных выборок
+
+print('Обучающая выборка параметров', x_train.shape)
+print('Обучающая выборка меток классов', y_train.shape)
+print()
+print('Тестовая выборка параметров', x_test.shape)
+print('Тестовая выборка меток классов', y_test.shape)
+
+
+def create_model():
+    # Создание модели
+    model = Sequential()
+
+    # Добавление слоев
+    model.add(Dense(60, input_dim=x_train.shape[1], activation='relu'))
+    model.add(Dense(30))
+    model.add(Activation('relu'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    # Компиляция и возврат модели
+    model.compile(loss='binary_crossentropy',
+                  optimizer=Adam(learning_rate=0.001),
+                  metrics=['accuracy'])
+
+    return model
+
+# Создание необученной модели при помощи функции create_model()
+model = create_model()
+
+# Обучение модели
+history = model.fit(x_train,       # Обучающая выборка параметров
+          y_train,       # Обучающая выборка меток класса
+          batch_size=8,  # Размер батча (пакета)
+          epochs=100,    # Количество эпох обучения
+          verbose=1)     # Отображение хода обучения
+
+
